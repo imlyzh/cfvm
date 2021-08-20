2901d660f416b09ceeb486868691a7eb572bef69
Lyzh <enterindex@gmail.com>
2021-08-21 02:08:56 +0800

    update basicblock structure

--- cfir/src/nodes/basicblock.rs
+++ cfir/src/nodes/basicblock.rs
@@ -3,10 +3,15 @@ use std::sync::Arc;
 use sexpr_ir::gast::Handle;
 use super::instruction::*;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct BlockMetadata {
+    pub label: Handle<String>,
+    pub prev_block: Arc<BasicBlockDef>,
+}
+
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BasicBlockDef {
-    pub label: Option<Handle<String>>,
-    pub dependencies: Vec<Arc<BasicBlockDef>>,
+    pub metadata: Option<BlockMetadata>,
     pub instructions: Vec<Arc<Instruction>>,
 }

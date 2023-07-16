2a3be7ece048a8bfa368ff46f50e14ed1d25c580
imlyzh <enterindex@gmail.com>
2023-07-17 00:08:56 +0800

    modify op, add region

--- fcir/src/block.rs
+++ fcir/src/block.rs
@@ -1,4 +1,11 @@
+use std::{collections::HashMap, ptr::NonNull};
+
 use crate::op::Op;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Region {
+  pub blocks: HashMap<Option<NonNull<str>>, Block>
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Block(pub Vec<Op>);
--- fcir/src/op.rs
+++ fcir/src/op.rs
@@ -1,13 +1,13 @@
 use std::{collections::HashMap, ptr::NonNull};
 
-use crate::{block::Block, types::FuncType, value::Value};
+use crate::{block::Region, types::FuncType, value::{Value, Constant}};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Op {
   pub opcode: NonNull<str>,
   pub output: Vec<NonNull<str>>,
   pub input:  Vec<Value>,
-  pub attr:   HashMap<NonNull<str>, ()>,
-  pub blocks: HashMap<Option<NonNull<str>>, Block>,
+  pub attr:   HashMap<NonNull<str>, Constant>,
+  pub ragion: Region,
   pub sign:   FuncType,
 }

711b78002b16c8e94d201aadc050089171eb7e9f
imlyzh <enterindex@gmail.com>
2023-10-04 13:12:18 +0800

    update machinecode

--- backend/src/MachineCode.rs
+++ backend/src/MachineCode.rs
@@ -1,5 +1,17 @@
+use cfvm_common::constant::Symbol;
 
+pub type MachineCodeList = Vec<MachineCode>;
 
-pub struct MachineCode (Symbol, );
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MachineCode (pub Symbol, Vec<Param>);
 
-// Predicate
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Param {
+  // literal symbol reference
+  LitSymRef(Symbol),
+  Reg(u64),
+  Lit1(u8),
+  Lit2(u16),
+  Lit4(u32),
+  Lit8(u64),
+}
